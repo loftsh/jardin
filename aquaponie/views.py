1fe2af288a54fb94daa71e0f7eb1e6387a9a542b
Théo Jacquin <42nimag@gmail.com>
2018-05-13 21:32:11 +0200

    Add temperature graph in dashboard

--- aquaponie/views.py
+++ aquaponie/views.py
@@ -10,7 +10,11 @@ from aquaponie.models import Temperature, WaterLevel
 logger = logging.getLogger(__name__)
 
 def allTemperature(request):
-    data = list(WaterLevel.objects.filter(bac='culture'))
+    data = list(Temperature.objects.all())
+    temperatures = [(str(d.date), d.temperature) for d in data]
+    temperatures = list(zip(*temperatures))
+
+    data = list(WaterLevel.objects.filter(bac='culture_med'))
     levels = [(str(d.date), d.level) for d in data]
     levels = list(zip(*levels))
 
@@ -21,6 +25,7 @@ def allTemperature(request):
     return render(
         request, "aquaponie/dashboard.html",
         context={
+            'temperatures': json.dumps(temperatures),
             'waterLevels': json.dumps(levels),
             'waterLevels2': json.dumps(levels2),
         }
